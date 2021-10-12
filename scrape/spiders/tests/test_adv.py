1a24a5f7a55911cdf36bd91644b8c77488833b3b
Henrique Lopes <riquellopes@gmail.com>
2021-10-12 04:42:00 -0300

    test: validated if class formrequest is call

--- scrape/spiders/tests/test_adv.py
+++ scrape/spiders/tests/test_adv.py
@@ -20,6 +20,11 @@ def dummy_zynga():
         url='https://br.advfn.com/bolsa-de-valores/bovespa/zynga-Z2NZ34/historico/mais-dados-historicos', body=read('zynga'))
 
 
+@pytest.fixture()
+def dummy_letter_b(): 
+    return HtmlResponse(
+        url='https://br.advfn.com/bolsa-de-valores/bovespa/B', body=read('letter_b'))
+
 def test_should_get_68_14_when_call_dor_stock(dummy_dor):
     spider = AdvSpider(limit=1)
     result = next(spider.parse_stock(dummy_dor))
@@ -37,3 +42,16 @@ def test_should_get_0_when_price_notfound(dummy_zynga):
         code='Z2NZ34', 
         last_price=0, 
         url='https://br.advfn.com/bolsa-de-valores/bovespa/zynga-Z2NZ34/historico/mais-dados-historicos') == result
+
+def test_should_call_with_b2w_data(dummy_letter_b, mocker):
+    form = mocker.patch('scrape.spiders.adv.FormRequest')
+
+    spider = AdvSpider(limit=1)
+    next(spider.parse(dummy_letter_b))
+
+    form.assert_called()
+    form.assert_called_with(
+        'https://br.advfn.com/bolsa-de-valores/bovespa/b2w-digital-on-BTOW1/historico/mais-dados-historicos',
+        callback=spider.parse_stock,
+        formdata={'Date1': '23/12/20', 'Date2': '23/12/20'}
+    )
