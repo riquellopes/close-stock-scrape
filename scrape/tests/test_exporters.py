43a2acebad21fa5a2428490a5a83e0db56e17b74
Henrique Lopes <riquellopes@gmail.com>
2021-10-18 13:01:39 -0300

    test: setting env to run test

--- scrape/tests/test_exporters.py
+++ scrape/tests/test_exporters.py
@@ -3,9 +3,15 @@ from moto import mock_s3
 from scrape.exporters import JsonS3Exporter
 
 @mock_s3
-def test_should_upload_file_to_s3_when_method_finish_to_call():
+def test_should_upload_file_to_s3_when_method_finish_to_call(mocker):
+    mocker.patch('scrape.exporters.get_project_settings', return_value={
+        'AWS_BUCKET': 'drummy',
+        'AWS_ACCESS_KEY_ID': '11111',
+        'AWS_SECRET_ACCESS_KEY': '********'
+    })
+
     conn = boto3.resource('s3', region_name='us-east-1')
-    conn.create_bucket(Bucket='dummy')
+    conn.create_bucket(Bucket='drummy')
 
     json = JsonS3Exporter('stocks_test')
     json.start_exporting()
@@ -13,5 +19,5 @@ def test_should_upload_file_to_s3_when_method_finish_to_call():
     json.export_item({"name": "moto"})
     json.finish_exporting()
 
-    body = conn.Object('dummy', 'stocks_test').get()['Body'].read().decode("utf-8")
+    body = conn.Object('drummy', 'stocks_test').get()['Body'].read().decode("utf-8")
     assert body == '[{"name": "moto"}]'
